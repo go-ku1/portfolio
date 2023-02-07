cea46f099672c631f0271253500ee0880040db3e
GOKUL <gokulkrishnarp00@gmail.com>
2023-02-07 23:21:20 +0530

    completed section4_header component

---
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { GET_SCREEN_INDEX, TOTAL_SCREENS } from "../../../utilities/commonUtils";
+import { faBars} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import ScrollService from "../../../utilities/ScrollService";
+import "./Header.css"
+
+export default function Header(){
+    const [selectedScreen,setSelectedScreen]=useState(0);
+    const [showHeaderoptions,setShowHeaderOptions]=useState(false);
+
+    const updateCurrentScreen=(currentScreen)=>{
+        if(!currentScreen || !currentScreen.screen.screenInView)
+            return;
+        let screenIndex=GET_SCREEN_INDEX(currentScreen.screenInView)
+        if(screenIndex<0)
+            return;
+    }
+    let currentScreenSubscription=ScrollService.CurrentScreenBroadCaster.subscribe(updateCurrentScreen)
+
+    const getHeaderOptions=()=>{
+        return(
+            TOTAL_SCREENS.map((screen,i)=>(
+                <div key={screen.screen_name} className={getHeaderOptionClass(i)} onClick={()=>switchScreen(i,screen)}>
+                    <span>{screen.screen_name}</span>
+                </div>
+            )
+
+            )
+        )
+    }
+
+    const getHeaderOptionClass=(index)=>{
+        let classes="header=option"
+        if(index<TOTAL_SCREENS.length-1)
+            classes+="header-option-seperator"
+        if(selectedScreen===index)
+            classes+="selected-header-option"
+    }
+
+    const switchScreen=(index,screen)=>{
+        let screencomponent=document.getElementById(screen.screen_name)
+        if(!screencomponent)
+            return;
+        screencomponent.scrollIntoView({behavior:'smooth'})
+        setSelectedScreen(index);
+        setShowHeaderOptions(false);
+    }
+
+    return(
+        <div className="header-container" onClick={()=>setShowHeaderOptions(!showHeaderoptions)}>
+            <div className="header-parent">
+                <div className="header-hamburger" onClick={()=>setShowHeaderOptions(!showHeaderoptions)}>
+                    <FontAwesomeIcon className="header-hamburger-bars" icon={faBars}/>
+                </div>
+                <div className="header-logo">
+                    <span>~GKR~</span>
+                </div>
+                <div className={(showHeaderoptions)?"header-options show-hamburger-options":"header-options"}>
+                    {getHeaderOptions()}
+                </div>
+            </div> 
+        </div>
+    )
+}
+
--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -2,10 +2,12 @@ import './Home.css'
 import React from 'react'
 import Footer from './Footer/Footer'
 import Profile from './Profile/Profile'
+import Header from './Header/Header'
 
 export default function Home(){
     return(
         <div className='home-container'>
+            <Header/>
             <Profile/>
             <Footer/>
         </div>
